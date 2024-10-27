8e45691d35ef775ef86fd267f310027e75e5adac
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2024-10-27 22:08:19 +0100

    adapt test to the new tab group approach

--- tests/testthat/test-tab_group.R
+++ tests/testthat/test-tab_group.R
@@ -1,49 +1,3 @@
-test_that("flatten_srv_module_list handles a simple list", {
-  input_list <- list(
-    list(server = "server1", module_id = "module1"),
-    list(server = "server2", module_id = "module2")
-  )
-  expected_output <- list(
-    list(server = "server1", module_id = "module1"),
-    list(server = "server2", module_id = "module2")
-  )
-  expect_equal(flatten_srv_module_list(input_list), expected_output)
-})
-
-test_that("flatten_srv_module_list handles nested server collections", {
-  srv_collection <- list(
-    list(server = "server3", module_id = "module3"),
-    list(server = "server4", module_id = "module4")
-  )
-  class(srv_collection) <- "server_collection"
-
-  nested_srv_collection <- list(
-    list(server = "server2", module_id = "module2"),
-    list(module_id = "sub_level", server = srv_collection)
-  )
-  class(nested_srv_collection) <- "server_collection"
-
-  input_list <- list(
-    list(server = "server1", module_id = "module1"),
-    list(module_id = "top_level", server = nested_srv_collection)
-  )
-
-  expected_output <- list(
-    list(server = "server1", module_id = "module1"),
-    list(server = "server2", module_id = "module2"),
-    list(server = "server3", module_id = "module3"),
-    list(server = "server4", module_id = "module4")
-  )
-  expect_equal(flatten_srv_module_list(input_list), expected_output)
-})
-
-test_that("flatten_srv_module_list handles empty list", {
-  input_list <- list()
-  expected_output <- list()
-  expect_equal(flatten_srv_module_list(input_list), expected_output)
-})
-
-
 # app ----
 
 local({
@@ -59,9 +13,9 @@ local({
     app <- shinytest2::AppDriver$new(root_app$get_url())
 
     # Switch to Module tab
-    app$set_inputs("__tabset_0__" = "Module Tab")
+    app$set_inputs("__tabset_0__" = "__tabset_1__")
     app$wait_for_idle()
-    html_code <- app$get_html("#mod_tab")
+    html_code <- app$get_html("#__tabset_1__")
     expect_true(
       startsWith(html_code, "<ul class=\"nav nav-pills shiny-tab-input shiny-bound-input")
     )
@@ -86,11 +40,10 @@ local({
     app <- shinytest2::AppDriver$new(root_app$get_url())
 
     # Switch to Module tab
-    app$set_inputs("__tabset_0__" = "Module Tab")
-    app$set_inputs("mod_tab" = "Nested modules")
+    app$set_inputs("__tabset_0__" = "__tabset_1__")
+    app$set_inputs("__tabset_1__" = "__tabset_2__")
     app$wait_for_idle()
-    app$view()
-    html_code <- app$get_html("#nested_mod_tab")
+    html_code <- app$get_html("#__tabset_2__")
     expect_true(
       startsWith(html_code, "<ul class=\"nav nav-pills shiny-tab-input shiny-bound-input")
     )
@@ -103,7 +56,7 @@ local({
 
     v4 <- app$get_values(output = "mod4-text")[["output"]][["mod4-text"]]
     expect_equal(
-      v2, "306"
+      v4, "306"
     )
   })
 
@@ -112,13 +65,12 @@ local({
     app <- shinytest2::AppDriver$new(root_app$get_url())
 
     # Switch to Module tab
-    app$set_inputs("__tabset_0__" = "Module Tab")
-    app$set_inputs("mod_tab" = "Send and Receive 2")
+    app$set_inputs("__tabset_0__" = "__tabset_1__")
+    app$set_inputs("__tabset_1__" = "mod_rec_2")
     app$wait_for_idle()
-    app$set_inputs("mod_tab" = "Nested modules")
-    app$set_inputs("nested_mod_tab" = "Send and Receive 1")
+    app$set_inputs("__tabset_1__" = "__tabset_2__")
+    app$set_inputs("__tabset_2__" = "mod_rec_1")
     app$wait_for_idle()
-    app$view()
 
     v1 <- app$get_values(output = TRUE)[["output"]][["mod_rec_1-output"]]
     v2 <- app$get_values(output = TRUE)[["output"]][["mod_rec_2-output"]]
@@ -127,22 +79,24 @@ local({
     expect_equal(v2, "1")
   })
 
+
+  # 
   test_that("tab_group other modules can switch into nested tabs" |>
     vdoc[["add_spec"]](c(specs$tab_group$allows_switching)), {
     app <- shinytest2::AppDriver$new(root_app$get_url())
 
     # Switch to Module tab
-    app$set_inputs("__tabset_0__" = "Switch to nest")
+    app$set_inputs("__tabset_0__" = "mod_switch1")
     app$wait_for_idle()
-    app$click("mod_switch-switch")
+    app$click("mod_switch1-switch")
     app$wait_for_idle()
 
-    top <- app$get_values(input = TRUE)[["input"]][["__tabset_0__"]]
-    first <- app$get_values(input = TRUE)[["input"]][["mod_tab"]]
-    second <- app$get_values(input = TRUE)[["input"]][["nested_mod_tab"]]
+    t0 <- app$get_values(input = TRUE)[["input"]][["__tabset_0__"]]
+    t1 <- app$get_values(input = TRUE)[["input"]][["__tabset_1__"]]
+    t2 <- app$get_values(input = TRUE)[["input"]][["__tabset_2__"]]
 
-    expect_equal(top, "Module Tab")
-    expect_equal(first, "Nested modules")
-    expect_equal(second, "Simple5")
+    expect_equal(t0, "__tabset_1__")
+    expect_equal(t1, "__tabset_2__")
+    expect_equal(t2, "mod5")
   })
 })
