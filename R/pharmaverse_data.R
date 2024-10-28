3d8286f4ddbf69477520b94acd62d5c67fb9804a
ml-ebs-ext <157474668+ml-ebs-ext@users.noreply.github.com>
2024-10-28 10:47:13 +0100

    Update pharmaverse_data.R
    Typo

--- R/pharmaverse_data.R
+++ R/pharmaverse_data.R
@@ -2,7 +2,7 @@ get_pharmaverse_data <- function(dataset) {
   if (!rlang::is_installed("pharmaverseadam")) stop("Please, install.package('pharmaverseadam')")
 
   res <- list(adsl = pharmaverseadam::adsl, adae = pharmaverseadam::adae, adlb = pharmaverseadam::adlb)[[dataset]]
-  if (is.null(res)) stop("Uknown dataset")
+  if (is.null(res)) stop("Unknown dataset")
 
   for (col in names(res)) {
     if (is.character(res[[col]]) || endsWith(col, "CD")) {
