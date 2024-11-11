a63b1b96d74d867f90e80fdd4c56e30a4a6ca3e5
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2024-11-11 17:00:31 +0100

    adds mock and dataset metadata compute

--- R/filter.R
+++ R/filter.R
@@ -15,16 +15,20 @@ get_dataset_filters_info <- function(data, filter_data) {
 }
 
 get_dataset_filters_ui <- function(dataset_filters_info, ns) {
-  res <- vector(mode = "list", length = length(dataset_filters_info))
-  for (idx in seq_along(dataset_filters_info)) {
-    entry <- dataset_filters_info[[idx]]
-    res[[idx]] <- shiny::div(
-      id = entry[["id_cont"]],
-      class = "filter-control  filter-filters",
-      shiny::tags[["label"]](entry[["name"]]),
-      dv.filter::data_filter_ui(ns(entry[["id"]])),
-      shiny::hr(style = "border-top: 2px solid gray; height: 10px;")
-    )
+  if (!isTRUE(getOption("new_filter_switch"))) {
+    res <- vector(mode = "list", length = length(dataset_filters_info))
+    for (idx in seq_along(dataset_filters_info)) {
+      entry <- dataset_filters_info[[idx]]
+      res[[idx]] <- shiny::div(
+        id = entry[["id_cont"]],
+        class = "filter-control  filter-filters",
+        shiny::tags[["label"]](entry[["name"]]),
+        dv.filter::data_filter_ui(ns(entry[["id"]])),
+        shiny::hr(style = "border-top: 2px solid gray; height: 10px;")
+      )
+    }
+  } else {
+
   }
   return(res)
 }
@@ -62,4 +66,105 @@ get_dataset_filters_server <- function(datasets_filters_info, data_list) {
   }
 
   return(res)
+}
+
+
+# NEW FILTER
+
+get_single_filter_data <- function(data) {
+  nm_col <- names(data)
+  n_col <- length(nm_col)
+  res <- vector(mode = "list", length = n_col)
+
+  for (idx in seq_len(n_col)) {
+    name <- nm_col[[idx]]
+    col <- data[[name]]
+
+    l <- list(name = name)
+
+    if (is.character(col)) {
+      l[c("value", "type")] <- list(unique(col), "character")
+    } else if (is.factor(col)) {
+      l[c("value", "type")] <- list(levels(col), "factor")
+    } else if (is.integer(col)) {
+      l[c("value", "type")] <- list(list(min = min(col), max = max(col)), "integer")
+    } else if (is.double(col)) {
+      l[c("value", "type")] <- list(list(min = min(col), max = max(col)), "integer")
+    } else if (inherits(col, "POSIXct")) {
+      d <- as.Date(col)
+      l[c("value", "type")] <- list(list(min = min(d), max = max(d)), "Date")
+    } else if (inherits(col, "Date")) {
+      l[c("value", "type")] <- list(list(min = min(col), max = max(col)), "Date")
+    }
+    res[[idx]] <- l
+  }
+  return(res)
+}
+
+get_filter_data <- function(datasets) {
+  nm_dataset <- names(datasets)
+  n_dataset <- length(nm_dataset)
+  res <- vector(mode = "list", length = n_dataset)
+  names(res) <- nm_dataset
+  for (idx in seq_len(n_dataset)) {
+    dataset <- datasets[[idx]]
+    nm_tables <- names(dataset)
+    n_tables <- length(nm_tables)
+    dataset_res <- vector(mode = "list", length = n_tables)
+    names(dataset_res) <- nm_tables
+    for (jdx in seq_len(n_tables)) {
+      table <- dataset[[jdx]]
+      dataset_res[[jdx]] <- get_single_filter_data(table)
+    }
+    res[[idx]] <- dataset_res
+  }
+  return(res)
+}
+
+new_filter_ui <- function(id, data) {
+  ns <- shiny::NS(id)
+  list(
+    shiny::h1("FILTER"),
+    shiny::div(
+      id = ns("filter_container"),
+      shiny::tags[["script"]](
+        type = "application/json",
+        jsonlite::toJSON(get_filter_data(data))
+      )
+    )
+  )
+}
+
+new_filter_server <- function(id, data) {
+  res <- shiny::reactive({
+    data()
+  })
+  return(res)
+}
+
+mock_new_filter <- function() {
+  data <- list(
+    D1 = list(cars = mtcars, iris = iris)
+  )
+
+  ui <- function(request) {
+    shiny::fluidPage(
+      shiny::bookmarkButton(),
+      new_filter_ui("filter", data),
+      shiny::verbatimTextOutput("output")
+    )
+  }
+
+  server <- function(input, output, session) {
+    x <- new_filter_server("filter", shiny::reactive(data))
+    output[["output"]] <- shiny::renderPrint({
+      x()
+    })
+  }
+
+  shiny::shinyApp(
+    ui = ui,
+    server = server,
+    enableBookmarking = "url"
+  )
 }
