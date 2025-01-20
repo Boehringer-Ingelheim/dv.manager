a1615faf6a9a5a46a1099e327de029827a4c3494
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2025-01-20 13:27:26 +0100

    update tooltip to reflect the show/hide of dataset filters

--- R/aaaa_info_strings.R
+++ R/aaaa_info_strings.R
@@ -5,7 +5,7 @@ TT <- local({
     "Apply a filter to the dataset and use the resulting subject IDs (default) to consistently filter the rest of datasets."
 
   DATASET_FILTER <-
-    "Apply a filter to an specific dataset. Does not impact the rest of datasets."
+    "Apply a filter to an specific dataset. Does not impact the rest of datasets. Only datasets that are used by the currently selected module are shown in this dataset."
 
   poc(
     SUBJECT_LEVEL_FILTER = SUBJECT_LEVEL_FILTER,
