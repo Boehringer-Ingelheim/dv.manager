327b5a20eeb58c69e4697c967793a08e8d7f10aa
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2025-03-26 12:56:24 +0100

    clean js code for selectors

--- filter_js/src/range_slider.js
+++ filter_js/src/range_slider.js
@@ -14,77 +14,45 @@ export class rangeSliderField extends Blockly.FieldNumber {
     /**
      * Class for an number slider field.
      *
-     * @param value The initial value of the field. Should
-     *    cast to a number. Defaults to 0.
+     * @param value The initial value of the field.
      * @param min Minimum value.
      * @param max Maximum value.
-     * @param precision Precision for value.
-     * @param validator A function that is called to validate
-     *    changes to the field's value. Takes in a number & returns a validated
-     *    number, or null to abort the change.
-     * @param config A map of options used to configure the field.
-     *    See the [field creation documentation]{@link
-     * https://developers.google.com/blockly/guides/create-custom-blocks/fields/built-in-fields/number#creation}
-     *    for a list of properties this parameter supports.
+     * @param precision Precision for value.     
      */
-    constructor(value, min, max, precision, validator, config) {
-        super(value, min, max, precision, validator, config);
-        /**
-         * Array holding info needed to unbind events.
-         * Used for disposing.
-         * Ex: [[node, name, func], [node, name, func]].
-         */
-        this.boundEvents = [];
+    constructor(value, min, max, precision) {
+        super(value, min, max, precision);
         /**
          * The HTML range input element.
          */
         this.sliderInput = null;
     }
-    /**
-     * Constructs a FieldSlider from a JSON arg object.
-     *
-     * @param options A JSON object with options
-     *     (value, min, max, precision).
-     * @returns The new field instance.
-     * @package
-     * @nocollapse
-     */
-    static fromJson(options) {
-        // `this` might be a subclass of FieldSlider if that class doesn't override
-        // the static fromJson method.
-        return new this(options.value, undefined, undefined, undefined, undefined, options);
-    }
+
     /* eslint-disable @typescript-eslint/naming-convention */
     /**
-     * Show the inline free-text editor on top of the text along with the slider
-     * editor.
+     * Creates dropdown
      *
      * @param e Optional mouse event that triggered the field to
-     *     open, or undefined if triggered programmatically.
-     * @param quietInput Quiet input (prevent focusing on the editor).
+     *     open, or undefined if triggered programmatically.     
      */
     showEditor_(e, quietInput) {
-        // Always quiet the input for the super constructor, as we don't want to
-        // focus on the text field, and we don't want to display the modal
-        // editor on mobile devices.
         super.showEditor_(e, true);
         // Build the DOM.
         const editor = this.dropdownCreate_();
         Blockly.DropDownDiv.getContentDiv().appendChild(editor);
-        
+
         const sourceBlock = this.getSourceBlock();
         if (sourceBlock instanceof Blockly.BlockSvg) {
             const primary = "white";
             const tertiary = "gray";
             Blockly.DropDownDiv.setColour(primary, tertiary);
         }
-        Blockly.DropDownDiv.showPositionedByBlock(this, sourceBlock, this.dropdownDispose_.bind(this));    
+        Blockly.DropDownDiv.showPositionedByBlock(this, sourceBlock, this.dropdownDispose_.bind(this));
 
         // Wait until we are outside of the stack for initialize
         setTimeout(() => {
-          this.initIonSlider_();
+            this.initIonSlider_();
         }, 0);
-        
+
         // Focus on the slider field, unless quietInput is passed.
         if (!quietInput) {
             editor.firstChild.focus({
@@ -92,7 +60,7 @@ export class rangeSliderField extends Blockly.FieldNumber {
             });
         }
     }
-    
+
     /**
      * Creates the slider editor and add event listeners.
      *
@@ -105,39 +73,35 @@ export class rangeSliderField extends Blockly.FieldNumber {
         wrapper.style.width = "400px";
         const internal = document.createElement('div');
         internal.style.backgroundColor = "white";
-        const sliderInput = document.createElement('input');        
+        const sliderInput = document.createElement('input');
         wrapper.appendChild(internal);
         internal.appendChild(sliderInput);
-        this.sliderInput = sliderInput;        
+        this.sliderInput = sliderInput;
         return wrapper;
     }
 
     initIonSlider_() {
-      if (!this.sliderInput) return;
+        if (!this.sliderInput) return;
 
-      $(this.sliderInput).ionRangeSlider({
-          min: this.min_,          
-          max: this.max_,
-          type: "double",
-          step: this.precision_,
-          from: this.getSourceBlock().getFieldValue("min"),
-          to: this.getSourceBlock().getFieldValue("max"),
-          skin: "shiny",
-          grid: "true",
-          onChange: (data) => this.onSliderChange_(data),          
-      });
+        $(this.sliderInput).ionRangeSlider({
+            min: this.min_,
+            max: this.max_,
+            type: "double",
+            step: this.precision_,
+            from: this.getSourceBlock().getFieldValue("min"),
+            to: this.getSourceBlock().getFieldValue("max"),
+            skin: "shiny",
+            grid: "true",
+            onChange: (data) => this.onSliderChange_(data),
+        });
 
-      this.ionSliderInstance = $(this.sliderInput).data("ionRangeSlider");
-  }
+        this.ionSliderInstance = $(this.sliderInput).data("ionRangeSlider");
+    }
 
     /**
      * Disposes of events belonging to the slider editor.
      */
     dropdownDispose_() {
-        for (const event of this.boundEvents) {
-            Blockly.browserEvents.unbind(event);
-        }
-        this.boundEvents.length = 0;
         this.ionSliderInstance.destroy();
         this.ionSliderInstance = null;
         this.sliderInput = null;
@@ -146,12 +110,12 @@ export class rangeSliderField extends Blockly.FieldNumber {
      * Sets the text to match the slider's position.
      */
     onSliderChange_(data) {
-      /* Refine by guessing which field is being updated */
-      this.getSourceBlock().getField("min").setEditorValue_(Number(data.from), false);
-      this.getSourceBlock().getField("max").setEditorValue_(Number(data.to), false);
-      this.getSourceBlock().setFieldValue(Number(data.from), "min");
-      this.getSourceBlock().setFieldValue(Number(data.to), "max");
-      this.resizeEditor_();
+        /* Refine by guessing which field is being updated */
+        this.getSourceBlock().getField("min").setEditorValue_(Number(data.from), false);
+        this.getSourceBlock().getField("max").setEditorValue_(Number(data.to), false);
+        this.getSourceBlock().setFieldValue(Number(data.from), "min");
+        this.getSourceBlock().setFieldValue(Number(data.to), "max");
+        this.resizeEditor_();
     }
     /**
      * Updates the slider when the field rerenders.
@@ -164,5 +128,5 @@ export class rangeSliderField extends Blockly.FieldNumber {
     }
 }
 Blockly.fieldRegistry.register('field_slider', rangeSliderField);
- 
+
 //# sourceMappingURL=field_slider.js.map
