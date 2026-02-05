9a2ab1bb47cba00a8d7a196c597b9432d7fdfd0e
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2026-02-05 13:52:06 +0100

    remove or update outdated docs

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface using an HTML template
 #'
-#' @param id This parameter can be either an string id if the app_ui is being used as a module or
+#' @param request_id This parameter can be either an string id if the app_ui is being used as a module or
 #' a request if the app_ui is being used as an standalone application then it becomes
 #' a Internal parameter for `{shiny}`
 #'     DO NOT REMOVE.
