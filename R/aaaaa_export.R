f4790037e3a3e58960c1461574abc4df1e889fe7
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2026-08-19 12:11:48 +0200

    replace mono font by a more common;y-bundled one

--- R/aaaaa_export.R
+++ R/aaaaa_export.R
@@ -136,7 +136,7 @@ output:
     number_sections: false
     df_print: kable
     latex_engine: xelatex
-    pandoc_args: ["-V", "monofont:DejaVu Sans Mono"]
+    pandoc_args: ["-V", "monofont:Latin Modern Mono"]
     extra_dependencies: ["pdflscape"]
     includes:
       in_header: header.tex
